4eae3fdb2411a2944128530c4e8f4e1bd23b09ff
VovaSv <vovasvidi@gmail.com>
2024-10-06 17:56:34 +0300

    Initial styling

---
+++ pizza-app/src/components/Button/Button.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import styles from './Button.module.css';
+import { ButtonProps } from './Button.props';
+import cn from 'classnames';
+
+export const ButtonAlt: FC<ButtonProps> = ({
+	className,
+	children,
+	...props
+}) => {
+	return (
+		<button
+			className={cn(styles['button'], styles['accent'], className)}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+};
+
+function Button({
+	children,
+	className,
+	appearance = 'small',
+	...props
+}: ButtonProps) {
+	return (
+		<button
+			className={cn(styles['button'], styles['accent'], className, {
+				[styles['small']]: appearance === 'small',
+				[styles['big']]: appearance === 'big',
+			})}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+}
+
+export default Button;

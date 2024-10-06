7835761c6b2d3bc9156bb030a812ee47a7107f3c
VovaSv <vovasvidi@gmail.com>
2024-10-06 18:56:36 +0300

    init routing configuration

--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import CartPage from './pages/Cart/Cart.tsx';
+import MenuPage from './pages/Menu/Menu.tsx';
+import MenuLayout from './layouts/Menu/Menu.tsx';
+import ErrorPage from './pages/Error/Error.tsx';
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <MenuLayout />,
+		children: [
+			{
+				path: '/',
+				element: <MenuPage />,
+			},
+			{
+				path: '/cart',
+				element: <CartPage />,
+			},
+		],
+	},
+	{
+		path: '*',
+		element: <ErrorPage />,
+	},
+]);
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>
 );
---
+++ pizza-app/src/pages/Cart/Cart.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+export default function Cart() {
+	return (
+		<div>
+			Cart Page
+		</div>
+	);
+}
